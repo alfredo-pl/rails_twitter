ea00b4a1c6b4b070a99da0d57b885c0f9427add5
alfredopl <aalfredoplz@gmail.com>
2021-02-10 09:08:29 -0300

    Revert "tweet null true"
    This reverts commit 7fb13d82efd7163040baa2280eeac81453b254f3.

--- db/migrate/20210210004057_create_tweets.rb
+++ db/migrate/20210210004057_create_tweets.rb
@@ -3,7 +3,7 @@ class CreateTweets < ActiveRecord::Migration[6.0]
     create_table :tweets do |t|
       t.string :content
       t.references :user, null: false, foreign_key: true
-      t.references :tweet, foreign_key: true
+      t.references :tweet, null: false, foreign_key: true
 
       t.timestamps
     end
