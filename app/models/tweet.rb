4944c8dddbe56f9465b713854a7e2f01d6fa9f8a
alfredopl <aalfredoplz@gmail.com>
2021-02-10 21:49:12 -0300

    method photo_user

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,4 +3,10 @@ class Tweet < ApplicationRecord
   #belongs_to :tweet
   #has_many :tweets
   validates :content, presence: true
+
+  def photo_user
+   photo = User.references(:tweets).where(id: user_id).pluck :photo
+    photo.first
+  end
+
 end
