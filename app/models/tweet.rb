99e4e593aa96a26c3045d5b5b95dc35d325d5947
alfredopl <aalfredoplz@gmail.com>
2021-02-14 14:29:34 -0300

    has many likes

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,12 +1,13 @@
 class Tweet < ApplicationRecord
   belongs_to :user
   #belongs_to :tweet
-  #has_many :tweets
+  has_many :likes
   validates :content, presence: true
 
   def photo_user
    photo = User.references(:tweets).where(id: user_id).pluck :photo
     photo.first
   end
+
 
 end
