b6965d5aa6946625d5bc34838c607afcd61779aa
alfredopl <aalfredoplz@gmail.com>
2021-02-14 14:30:29 -0300

    routes for likes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :tweets
+  resources :tweets do
+  resources :likes
+end
   devise_for :users , controllers: {
     registrations: 'users/registrations'
   }
   root 'tweets#index'
+ 
+
 end
