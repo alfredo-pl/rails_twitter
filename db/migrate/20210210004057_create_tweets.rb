a0e85a1e3bc5a943aa2c847b4a1887b7b4813a94
alfredopl <aalfredoplz@gmail.com>
2021-02-14 14:31:02 -0300

    migrations create table likes

--- db/migrate/20210210004057_create_tweets.rb
+++ db/migrate/20210210004057_create_tweets.rb
@@ -3,11 +3,7 @@ class CreateTweets < ActiveRecord::Migration[6.0]
     create_table :tweets do |t|
       t.string :content
       t.references :user, null: false, foreign_key: true
-<<<<<<< HEAD
       t.references :tweet, null: false,foreign_key: true
-=======
-      t.references :tweet, null: false, foreign_key: true
->>>>>>> ea00b4a1c6b4b070a99da0d57b885c0f9427add5
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_10_004057) do
+ActiveRecord::Schema.define(version: 2021_02_14_145051) do
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "tweet_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["tweet_id"], name: "index_likes_on_tweet_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.string "content"
     t.integer "user_id", null: false
-    t.integer "tweet_id", null: false
+    t.integer "tweet_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["tweet_id"], name: "index_tweets_on_tweet_id"
@@ -36,6 +45,8 @@ ActiveRecord::Schema.define(version: 2021_02_10_004057) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "likes", "tweets"
+  add_foreign_key "likes", "users"
   add_foreign_key "tweets", "tweets"
   add_foreign_key "tweets", "users"
 end
