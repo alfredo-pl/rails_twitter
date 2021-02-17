6037b6a61b97be542b3e5a51f0cd92e1672c77fb
alfredopl <aalfredoplz@gmail.com>
2021-02-17 12:04:40 -0300

    methods profile retweet

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,13 +1,32 @@
 class Tweet < ApplicationRecord
   belongs_to :user
-  #belongs_to :tweet
-  has_many :likes
-  validates :content, presence: true
+  
+  has_many :likes , dependent: :destroy
+  has_many :retweets, class_name: 'Tweet', foreign_key: 'tweet_id' , dependent: :destroy
+  validates :content,  presence: true 
 
   def photo_user
    photo = User.references(:tweets).where(id: user_id).pluck :photo
     photo.first
   end
+  def name_user
+    name = User.references(:tweets).where(id: user_id).pluck :name
+     name.first
+   end
 
+  def perfil_retweet
+    tweet = Tweet.references(:tweets).where(id: tweet_id).pluck :user_id
+    photo = User.references(:tweets).where(id: tweet).pluck :photo
+    photo.first
+  end
+  def perfil_retweet_name
+    tweet = Tweet.references(:tweets).where(id: tweet_id).pluck :user_id
+    name = User.references(:tweets).where(id: tweet).pluck :name
+    name.first
+  end
+  def perfil_retweet_content
+    content = Tweet.references(:tweets).where(id: tweet_id).pluck :content
+    content.first
+  end
 
 end
