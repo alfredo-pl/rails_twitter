5da7bde34408fa12c319b171945f3ab2bfe8b69d
alfredopl <aalfredoplz@gmail.com>
2021-02-17 12:05:19 -0300

    dependet destroy tweets

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :tweets
+  has_many :tweets, dependent: :destroy
   has_many :likes, dependent: :destroy
   validates :name, :photo, presence: true
 end
