e7b9c7eba74ab072859cd4d421fb53fd2a1b4edb
alfredopl <aalfredoplz@gmail.com>
2021-02-17 12:10:18 -0300

    member retweet

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   resources :tweets do
-  resources :likes
-end
+    member do
+        post :retweet
+    end
+    resources :likes
+  end
   devise_for :users , controllers: {
     registrations: 'users/registrations'
   }
+  
   root 'tweets#index'
  
 
