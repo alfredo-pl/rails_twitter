10d3cc7ee840cde6e9d984e4eff77a731dcfff73
alfredopl <aalfredoplz@gmail.com>
2021-02-22 11:37:32 -0300

    Revert "g model friend"
    This reverts commit e5dadeab8ce01ba2b5acf7b92cf94d6d580d030c.

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_22_135348) do
-
-  create_table "friends", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "friend_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["friend_id"], name: "index_friends_on_friend_id"
-    t.index ["user_id"], name: "index_friends_on_user_id"
-  end
+ActiveRecord::Schema.define(version: 2021_02_14_145051) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -54,8 +45,6 @@ ActiveRecord::Schema.define(version: 2021_02_22_135348) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "friends", "friends"
-  add_foreign_key "friends", "users"
   add_foreign_key "likes", "tweets"
   add_foreign_key "likes", "users"
   add_foreign_key "tweets", "tweets"
