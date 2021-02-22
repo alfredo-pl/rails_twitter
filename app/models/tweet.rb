ae481515baf50483fa15aca2d13088b2a422ef13
alfredopl <aalfredoplz@gmail.com>
2021-02-22 18:02:41 -0300

    scope tweets for me

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,11 +4,18 @@ class Tweet < ApplicationRecord
   has_many :likes , dependent: :destroy
   has_many :retweets, class_name: 'Tweet', foreign_key: 'tweet_id' , dependent: :destroy
   validates :content,  presence: true 
+  
+  def self.tweets_for_me(current_user)
+    list_friends = Friend.references(:users).where(user_id: current_user)
+    friend = list_friends.map{|e|e.friend_id}
+   Tweet.where(user_id: friend)
+  end
 
   def photo_user
    photo = User.references(:tweets).where(id: user_id).pluck :photo
     photo.first
   end
+  
   def name_user
     name = User.references(:tweets).where(id: user_id).pluck :name
      name.first
@@ -19,11 +26,13 @@ class Tweet < ApplicationRecord
     photo = User.references(:tweets).where(id: tweet).pluck :photo
     photo.first
   end
+
   def perfil_retweet_name
     tweet = Tweet.references(:tweets).where(id: tweet_id).pluck :user_id
     name = User.references(:tweets).where(id: tweet).pluck :name
     name.first
   end
+  
   def perfil_retweet_content
     content = Tweet.references(:tweets).where(id: tweet_id).pluck :content
     content.first
