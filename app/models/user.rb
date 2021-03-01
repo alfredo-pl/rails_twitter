20692886364bed4524ec9bc4d68c0f2e232aa7bf
alfredopl <aalfredoplz@gmail.com>
2021-02-28 21:36:41 -0300

    methods and has many friends

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,24 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :tweets, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :friends, dependent: :destroy
   validates :name, :photo, presence: true
+
+  def friends_count
+    Friend.references(:users).where(user_id: id).count
+  end
+
+  def tweet_count
+    Tweet.references(:users).where(user_id: id).count
+  end
+
+  def like_count
+    Like.references(:users).where(user_id: id).count
+  end
+
+  def retweet_count
+    t = Tweet.references(:users).where(user_id: id)
+    r = t.where(tweet_id: nil).count
+    t.count - r
+  end
 end
