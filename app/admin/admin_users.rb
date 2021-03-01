184afcb391267a411222d327e7c9c9d9ed547e90
alfredopl <aalfredoplz@gmail.com>
2021-02-28 21:18:17 -0300

    colums and params to user

--- app/admin/admin_users.rb
+++ app/admin/admin_users.rb
@@ -1,24 +1,28 @@
-ActiveAdmin.register AdminUser do
-  permit_params :email, :password, :password_confirmation
+ActiveAdmin.register User do
+  permit_params :email, :name, :photo, :password, :password_confirmation
 
   index do
     selectable_column
     id_column
     column :email
-    column :current_sign_in_at
-    column :sign_in_count
+    column :friends_count
+    column :tweet_count
+    column :like_count
+    column :retweet_count
     column :created_at
     actions
   end
 
   filter :email
-  filter :current_sign_in_at
-  filter :sign_in_count
+  filter :friends
+
   filter :created_at
 
   form do |f|
     f.inputs do
       f.input :email
+      f.input :name
+      f.input :photo
       f.input :password
       f.input :password_confirmation
     end
